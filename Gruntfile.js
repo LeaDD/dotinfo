57c9c59db5768bd0b3af15bc7ae9f7871f1daf84
leadd <ddeal812@gmail.com>
2017-09-28 22:17:58 -0500

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt);
+
+    //Use the following line to import a .yml file which will name all of
+    //our directories we might want to reference in the Gruntfile.js. Helps with
+    //maintenance.
+
+    grunt.initConfig({
+        //Lint Javascript
+        jshint: {
+            all: [
+            'Gruntfile.js',
+            'src/**/*.js'
+            ]
+        },
+        //Minify Javascript
+        uglify: {
+            dist: {
+                //placeholder
+            }
+        },
+        //Minify HTML
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'dist/index.html': 'src/index.html'
+                }
+            }
+        },
+        //Minify CSS
+        cssmin: {
+            target: {
+                files: {
+                    'dist/css/style.css': 'src/css/style.css'
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default',
+        ['jshint',
+        'uglify',
+        'htmlmin',
+        'cssmin'
+        ]);
+};
